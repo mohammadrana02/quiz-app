44377c928b051466bb46ca0ed848be5e3d08a480
Mohammad Rana <mohammadrana02@gmail.com>
2023-06-08 16:17:21 +0100

    finished program

--- quiz_brain.py
+++ quiz_brain.py
@@ -20,13 +20,10 @@ class QuizBrain:
         # user_answer = input()
         # self.check_answer(user_answer)
 
-    def check_answer(self):
+    def check_answer(self, user_answer):
         correct_answer = self.current_question.answer
         if user_answer.lower() == correct_answer.lower():
             self.score += 1
-            print("You got it right!")
+            return True
         else:
-            print("That's wrong.")
-
-        print(f"Your current score is: {self.score}/{self.question_number}")
-        print("\n")
+            return False
--- ui.py
+++ ui.py
@@ -18,8 +18,8 @@ class QuizInterface:
 
         self.tick_image = PhotoImage(file="images/true.png")
         self.cross_image = PhotoImage(file="images/false.png")
-        self.tick = Button(image=self.tick_image, highlightthickness=0, command=self.true_answer)
-        self.cross = Button(image=self.cross_image, highlightthickness=0, command=self.false_answer)
+        self.tick = Button(image=self.tick_image, highlightthickness=0, command=self.true_pressed)
+        self.cross = Button(image=self.cross_image, highlightthickness=0, command=self.false_pressed)
         self.tick.grid(column=0, row=2)
         self.cross.grid(column=1, row=2)
 
@@ -31,14 +31,29 @@ class QuizInterface:
         self.window.mainloop()
 
     def get_next_question(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text=q_text)
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            self.score.config(text=f"Score: {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="You've reached the end of the quiz.")
+            self.tick.config(state="disabled")
+            self.cross.config(state="disabled")
 
-    def true_answer(self):
-        self.quiz.check_answer(True)
+    def true_pressed(self):
+        is_right = self.quiz.check_answer("True")
+        self.give_feedback(is_right)
 
-    def false_answer(self):
-        self.quiz.check_answer(False)
+    def false_pressed(self):
+        is_right = self.quiz.check_answer("False")
+        self.give_feedback(is_right)
 
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
 
+        else:
+            self.canvas.config(bg="red")
 
+        self.window.after(1000, self.get_next_question)
